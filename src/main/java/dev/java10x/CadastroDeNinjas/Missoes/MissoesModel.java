1cb4b7ce556e13db5d8ab8ea6f0359c1b041c014
mickaelulivieri <mmickael.carvalho@gmail.com>
2024-11-22 19:19:54 -0300

    Criando classes para declarar suas dependencias

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,12 +3,15 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.data.annotation.TypeAlias;
-
 import java.util.List;
 
 @Entity
 @Table(name = "tb_Missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,14 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_Ninjas_Cadastrados")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
-
     //notations para manipular id e entender como sera feito, no caso, numeros sequenciais
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
@@ -26,15 +29,5 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "Missoes_id")
     private MissoesModel missoes;
-
-    //CONSTRUTORES
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public NinjaModel() {
-    }
 
 }
