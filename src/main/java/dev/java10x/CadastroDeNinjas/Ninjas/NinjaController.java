69f8f059654fff1ab095b359c1f0b10c918e72ab
mickaelulivieri <mmickael.carvalho@gmail.com>
2024-11-28 16:54:07 -0300

    feat: atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,18 +1,44 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-//para dizer q a classe e um controlador e pede a notacao pra mapear as rotas
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")  // Para evitar o uso de rota genérica
 public class NinjaController {
 
-    //get e apenas um dos comandos q podem ser feitos
+    // Mensagem de boas-vindas
     @GetMapping("/boasVindas")
     public String boasVindas(){
-        return "Essa é minha primeira mensagem nessa rota!";
+        return "Olá Mundo";
     }
 
+    // Adicionar ninja (CREATE)
+    @PostMapping("/adicionar")
+    public String criarNinja(){
+        return "Olá Mundo";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Olá Mundo";
+    }
+
+    // Mostrar ninja por ID (READ)
+    @GetMapping("/mostrar/{id}")
+    public String mostrarNinjaPorId(@PathVariable Long id){
+        return "Olá Mundo";
+    }
+
+    // Alterar dados do ninja (UPDATE)
+    @PutMapping("/alterarId")
+    public String alterarNinjaPorID(){
+        return "Olá Mundo";
+    }
+
+    // Deletar ninja (DELETE)
+    @DeleteMapping("/delete/{id}")
+    public String deletarNinjaPorID(@PathVariable Long id){
+        return "Olá Mundo";
+    }
 }
